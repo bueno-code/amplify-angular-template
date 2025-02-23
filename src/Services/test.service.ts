205c1b72c7b4c1add6d705e2f866ae1ec9e5d8e8
bueno-code <cjsturge@hotmail.co.uk>
2025-02-23 03:42:24 +0000

    Add test service files

---
+++ src/Services/test.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService {
+
+  constructor(private http:HttpClient) { }
+
+  public test():Observable<string>
+  {
+    return this.http.get<string>(environment.API_URL + '/SimpleApi')
+  }
+}
