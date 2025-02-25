41b0780d5afd0711b68a956ab86eab511962704c
bueno-code <cjsturge@hotmail.co.uk>
2025-02-25 18:32:08 +0000

    Add cdkDrag directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 Amplify.configure(outputs);
 
@@ -11,7 +12,7 @@ Amplify.configure(outputs);
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent],
+  imports: [RouterOutlet, TodosComponent,DragDropModule],
 })
 export class AppComponent {
   title = 'amplify-angular-template';
